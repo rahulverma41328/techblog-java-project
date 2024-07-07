970b0f96c77025bb74bbbcd90cf298b007edccde
rahul verma <rahulverma41328@gmail.com>
2024-07-07 21:59:33 +0530

    rahul verma

---
+++ src/main/java/com/tech/blog/helper/ConnectionProvider.java
@@ -0,0 +1,25 @@
+package com.tech.blog.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+
+    private static Connection con;
+
+    public static Connection getCon(){
+        try {
+            // driver class
+           if (con==null){
+               Class.forName("com.mysql.cj.jdbc.Driver");
+
+               // create a connection
+               con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techblog","root","rahul");
+
+           }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
